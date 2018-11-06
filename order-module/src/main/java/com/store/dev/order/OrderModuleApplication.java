59a050ce3cd019afa424aba670695736e2c2bd5c
Ricrov <ric.hou@hotmail.com>
2018-11-06 16:53:34 +0800

    project initialized

---
+++ order-module/src/main/java/com/store/dev/order/OrderModuleApplication.java
@@ -0,0 +1,11 @@
+package com.store.dev.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderModuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderModuleApplication.class, args);
+    }
+}
