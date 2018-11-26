d9ffa5af53ed9c5765e8b0fb505ab7422174df5e
Lanou_铭 <1434640043@qq.com>
2018-11-26 14:48:33 +0800

    1111

--- cart-module/src/main/java/com/store/dev/cart/controller/CartController.java
+++ cart-module/src/main/java/com/store/dev/cart/controller/CartController.java
@@ -39,7 +39,7 @@ public class CartController {
     private Jedis jedis;
 
     // 查询所有用户的购物车
-    @RequestMapping("/findAll")
+    @GetMapping("/findAll")
     public List<UserEntity> findAll() {
         List<UserEntity> all = cartService.findAll();
         return all;
@@ -71,7 +71,6 @@ public class CartController {
                 map.put("goodsNumber", number);
                 redisCartList.add(map);
             }
-            System.out.println("****************************");
             System.out.println(redisCartList);
 
             return redisCartList;
@@ -86,39 +85,10 @@ public class CartController {
         return getResultWrapper(result);
     }
 
-    // 插入用户购买的商品(用户ID,商品ID,商品数量)
-//    @ApiOperation(value = "向购物车中插入用户购买的商品", notes = "接收json类型数据")
-//    @RequestMapping("/addGoodsTest")
-//    public ResultWrapper addUserCartGoods(@RequestBody CartEntity cartEntity) {
-//        pool = new JedisPool();
-//        jedis = pool.getResource();
-//        if (cartEntity.getUserId() != null && !"".equals(cartEntity.getUserId())) {
-//            Integer result = cartService.addUserCartGoods(cartEntity);
-//            return getResultWrapper(result);
-//        } else {
-//            String userIP = GetUserIp.getIpAddr(request);
-//            System.out.println(userIP);
-//            String itemId = String.valueOf(cartEntity.getItemId());
-//            String goodsNumber = String.valueOf(cartEntity.getGoodsNumber());
-//            System.out.println(itemId);
-//            System.out.println(goodsNumber);
-////            Boolean exists = jedis.exists(userIP);
-//            Map<String, String> map = new HashMap<>();
-//            map.put("itemId", itemId);
-//            map.put("goodsNumber", goodsNumber);
-//            String s = jedis.hmset(userIP, map);
-//            return getResultWrapper(s);
-//        }
-//    }
-
-//    @RequestMapping("/redisCart")
-//    public List<Map<String, Object>> getRedisCart() {
-//
-//    }
 
     @ApiOperation(value = "根据用户ID和商品ID更新购物车中的商品数量", notes = "接收json数据")
     // 更新购物车中的商品数量
-    @RequestMapping("/updateGoodsNumber")
+    @PostMapping("/updateGoodsNumber")
     public ResultWrapper updateGoodsNumber(@RequestBody CartEntity cartEntity) {
         Integer result = cartService.updateGoodsNumber(cartEntity);
         return getResultWrapper(result);
@@ -135,7 +105,7 @@ public class CartController {
 
     @ApiOperation(value = "根据用户ID和商品ID的集合批量删除购物车中的商品", notes = "接收json数据,例子: Map<String, ArrayList<Integer>> itemIds")
     // 批量删除购物车商品
-    @RequestMapping("/deleteItems")
+    @PostMapping("/deleteItems")
     public ResultWrapper deleteGoods(@RequestBody Map<String, ArrayList<Integer>> itemIds) {
         ArrayList<Integer> list = itemIds.get("itemIds");
         ResultWrapper wrapper = cartService.deleteGoods(7L, list);
